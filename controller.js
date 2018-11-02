87dc14b126f034049c2d736959e4eab326015491
ComeAlongErica <ericaraephoto@gmail.com>
2018-11-02 15:21:48 -0400

    add functionality

--- controller.js
+++ controller.js
@@ -27,12 +27,19 @@ function TodoController() {
     vm.addTodo = (addTodo) => {
         vm.todoList.push(angular.copy({todo: addTodo, completed: false}));
         console.log(vm.todoList);
-    };
+    }
 
     vm.todoCompleted = (index, todo, completed) => {
         vm.todoList.splice(index, {todo, completed});
-    };
+    }
 
+    vm.removeTodo = (index)  => {
+        vm.todoList.splice(index, 1);
+    }
+
+    vm.addClassCompleted = (index, todoItem)  => {
+        vm.todoList.splice(index, 1, {todo: todoItem, completed: true});
+    }
 }
 
 // getter syntax
