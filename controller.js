deedcbf2b7c2375bac4ec66be4c40b57599aa1f0
ComeAlongErica <ericaraephoto@gmail.com>
2018-11-05 19:02:09 -0500

    clean up some things

--- controller.js
+++ controller.js
@@ -3,50 +3,41 @@ function TodoController() {
     const vm = this; //vm stands for View Model
 
     vm.todoList = [
-
         {
             todo: "be sexy",
             completed: false
-
-        },
-        {
+        }, {
             todo: "don't get murdered",
             completed: true
-        },
-        {
+        }, {
             todo: "be happy",
             completed: false
-        },
-        {
+        }, {
             todo: "take your vitamins",
             completed: true
-
         }
     ];
 
+    //push to task list
     vm.addTodo = (addTodo) => {
         vm.todoList.push(angular.copy({ todo: addTodo, completed: false }));
     }
 
-    vm.todoCompleted = (index, todo, completed) => {
-        vm.todoList.splice(index, { todo, completed });
-    }
-
+    // remove task from array
     vm.removeTodo = (index) => {
         vm.todoList.splice(index, 1);
     }
 
+    // add boolean of true to completed
     vm.addClassCompleted = (index) => {
         vm.todoList[index].completed = true
     }
 
-
+    // edit a task
     vm.editTask = (index, value) => {
-        let todo = vm.todoList[index].todo;;
-        if (value !== null){
-          vm.todoList[index].todo = value;
-        }
-      }
+        vm.todoList[index].todo = value;
+    }
+
 }
 
 // getter syntax
